c3d90b35ce33ffef9a05c997305c4548d828056e
Brandon Do <mbdx97@gmail.com>
2020-09-25 23:33:53 -0400

    home component now has simple-stat as child component

--- src/app/simple-stat/simple-stat.component.ts
+++ src/app/simple-stat/simple-stat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { IPlayerData } from 'src/interfaces/IPlayerData';
 import { IMatchHistory } from 'src/interfaces/IMatchHistory';
 import { FaceitService } from '../../services/faceit.service';
@@ -8,25 +8,30 @@ import { FaceitService } from '../../services/faceit.service';
   templateUrl: './simple-stat.component.html',
   styleUrls: ['./simple-stat.component.css']
 })
-export class SimpleStatComponent implements OnInit {
+export class SimpleStatComponent implements OnChanges {
 
+  @Input() username: string;
   playerData: IPlayerData;
   matchHistory: IMatchHistory;
 
   constructor(private faceitService: FaceitService) { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.getData(this.username);
   }
 
-  getData(username: string): void {
-    console.log(username);
+  private getData(username: string): void {
+    this.getPlayerData(username);
+  }
+
+  private getPlayerData(username: string): void {
     this.faceitService.getData(username).subscribe(data => {
       this.playerData = data;
       this.getMatchHistory(this.playerData.player_id);
     });
   }
 
-  getMatchHistory(userId: string): void {
+  private getMatchHistory(userId: string): void {
     this.faceitService.getMatchHistory(userId).subscribe(data => {
       this.matchHistory = data;
     });
